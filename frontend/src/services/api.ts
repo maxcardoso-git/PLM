fb52eb2e00d45acbcd87284250b4525f818ac40a
Max Cardoso <max.dxca@gmail.com>
2026-01-02 22:22:21 -0300

    Add withCredentials to axios for cookie auth

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -38,6 +38,7 @@ class ApiClient {
       headers: {
         'Content-Type': 'application/json',
       },
+      withCredentials: true,
     });
 
     this.client.interceptors.request.use((config) => {
